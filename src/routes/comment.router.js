d37657869f7bc9c556f6e108224e91f607e438e1
julie kim <julie.kim.dev@gmail.com>
2024-02-08 15:56:51 +0900

    댓글 작성 유효성 검사 보완

--- src/routes/comment.router.js
+++ src/routes/comment.router.js
@@ -13,8 +13,16 @@ router.post(
     const { userId } = req.user;
 
     const post = await prisma.posts.findFirst({ where: { boardId: +boardId } });
+
+    // 댓글 달 게시글 존재x
     if (!board) {
       return res.status(404).json({ message: "게시글이 존재하지 않습니다." });
+    }
+    // 댓글 내용 존재x
+    if (!content) {
+      return res
+        .status(400)
+        .json({ message: "작성된 내용이 존재하지 않습니다." });
     }
 
     const comment = await prisma.comments.create({
