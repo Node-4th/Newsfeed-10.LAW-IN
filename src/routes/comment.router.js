186f058706f5ea9ece38497f917ddb9b198dc29b
julie kim <julie.kim.dev@gmail.com>
2024-02-08 20:09:15 +0900

    댓글 수정 구현 완료

--- src/routes/comment.router.js
+++ src/routes/comment.router.js
@@ -43,7 +43,6 @@ router.post(
 router.get("/boards/:boardId/comments"),
   async (req, res, next) => {
     const { boardId } = req.params;
-    const { nickname } = req.body;
 
     const comments = await prisma.comments.findMany({
       where: { boardId: +boardId },
@@ -60,12 +59,59 @@ router.get("/boards/:boardId/comments"),
       orderBy: { createdAt: "desc" },
     });
 
-    return res
-      .status(200)
-      .json({
-        success: "댓글 조회가 성공적으로 진행되었습니다.",
-        data: comments,
-      });
+    return res.status(200).json({
+      success: "댓글 조회가 성공적으로 진행되었습니다.",
+      data: comments,
+    });
   };
+
+// 댓글 수정
+router.patch(
+  "/boards/:boardId/comments/:id",
+  authMiddleware,
+  async (req, res, next) => {
+    const boardId = req.params.boardId;
+    const { content } = req.body;
+    if (!boardId) {
+      return res.status(400).json({
+        success: "boardId는 필수값입니다.",
+      });
+    }
+    if (!content) {
+      return res.status(400).json({
+        success: "작성된 내용이 존재하지 않습니다.",
+      });
+    }
+
+    const board = await prisma.board.findFirst({
+      where: {
+        boardId: +boardId,
+      },
+    });
+
+    if (!board) {
+      return res.status(400).json({
+        success: "게시글이 존재하지 않습니다.",
+      });
+    }
+
+    if (board.userId !== user.userId) {
+      return res.status(400).json({
+        success: "올바르지 않은 요청입니다.",
+      });
+    }
+
+    await prisma.board.update({
+      where: {
+        boardId: +boardId,
+      },
+      data: {
+        content,
+      },
+    });
+
+    return res.status(201).json({ success: "댓글 수정이 완료되었습니다." });
+  }
+);
 
 export default router;
