5a935a796160495a9d39123dccd2a1aaca4b9927
wltjsl <ljss7291@gmail.com>
2024-02-14 12:48:08 +0900

    함수화의 흔적

--- src/routes/features.router.js
+++ src/routes/features.router.js
@@ -3,75 +3,150 @@ import AuthMiddleware from "../middlewares/auth.middleware.js";
 import { prisma } from "../utils/prisma/index.js";
 const router = express.Router();
 
+const existCheck = async (userId, propId, tableName, columnName) => {
+  const isExist = await prisma[tableName].findFirst({
+    where: {
+      userId: userId,
+      [columnName]: propId,
+    },
+  });
+  console.log("isExist => ", isExist);
+  return isExist;
+};
+
+const canceler = async (userId, propId, tableName, columnName) => {
+  await prisma[tableName].delete({
+    where: {
+      [`userId_${columnName}`]: {
+        userId: userId,
+        [columnName]: propId,
+      },
+    },
+  });
+};
+
+const creator = async (userId, propId, tableName, columnName) => {
+  await prisma[tableName].create({
+    data: {
+      userId: userId,
+      [columnName]: BigInt(propId),
+    },
+  });
+};
+
+const counter = async (propId, tableName, columnName) => {
+  return await prisma[tableName].count({
+    where: {
+      [columnName]: propId,
+    },
+  });
+};
+
+const userUpdated = async (propId, tableName, columnName, count) => {
+  return await prisma[tableName].update({
+    where: {
+      id: BigInt(propId),
+    },
+    data: {
+      [columnName]: BigInt(count),
+    },
+  });
+};
+
+// const handleGood = async (userId, propId, tableName, tableName2, columnName, res,  , message) => {
+//   console.log("isExist => ", isExist);
+
+//   if (isExist) {
+//     await prisma[tableName].delete({
+//       where: {
+//         [`userId_${columnName}`]: {
+//           userId: userId,
+//           [columnName]: propId,
+//         },
+//       },
+//     });
+
+//     const count = await prisma[tableName].count({
+//       where: {
+//         [columnName]: propId,
+//       },
+//     });
+//     console.log("count => ", count);
+
+//     const updatedUser = await prisma[tableName2].update({
+//       where: {
+//         id: BigInt(propId),
+//       },
+//       data: {
+//         like: BigInt(count),
+//       },
+//     });
+//     console.log("updatedUser => ", updatedUser);
+
+//     return res.status(400).json({ errorMessage: message + "를 취소하였습니다." });
+//   }
+
+// await prisma[tableName].create({
+//   data: {
+//     userId: userId,
+//     [columnName]: BigInt(propId),
+//   },
+// });
+
+//   console.log("생성완료");
+
+//   const count = await prisma[tableName].count({
+//     where: {
+//       [columnName]: propId,
+//     },
+//   });
+
+//   console.log("count => ", count);
+
+//   const updatedUser = await prisma[tableName2].update({
+//     where: {
+//       id: BigInt(propId),
+//     },
+//     data: {
+//       like: BigInt(count),
+//     },
+//   });
+
+//   console.log("updatedUser => ", updatedUser);
+
+//   return res.status(201).json({ message: message + "가 추가 되었습니다." });
+// };
+
 // 좋아요
 router.post("/boards/:boardId/comments/:commentsId/likes", AuthMiddleware, async (req, res) => {
   // 유저아이디 받아오기(좋아요 누를 인원 , 인증검사 진행후)
   const { id } = req.user;
   const commentsId = req.params.commentsId;
 
-  const isExistLike = await prisma.liked.findFirst({
-    where: {
-      userId: id,
-      likedCommentId: commentsId,
-    },
-  });
+  const isExistLike = existCheck(id, commentsId, "liked", "likedCommentId");
 
   console.log("isExistLike => ", isExistLike);
 
   if (isExistLike) {
-    await prisma.liked.delete({
-      where: {
-        userId_likedCommentId: {
-          userId: id,
-          likedCommentId: commentsId,
-        },
-      },
-    });
+    canceler(id, commentsId, "liked", "likedCommentId");
 
-    const count = await prisma.liked.count({
-      where: {
-        likedCommentId: commentsId,
-      },
-    });
+    const count = counter(commentsId, "liked", "likedCommentsId");
     console.log("count => ", count);
 
-    const updatedUser = await prisma.comments.update({
-      where: {
-        id: BigInt(commentsId),
-      },
-      data: {
-        like: BigInt(count),
-      },
-    });
+    const updatedUser = userUpdated(commentsId, "liked", "like", count);
     console.log("updatedUser => ", updatedUser);
+
     return res.status(400).json({ errorMessage: "좋아요를 취소하였습니다." });
   }
 
-  await prisma.liked.create({
-    data: {
-      userId: id,
-      likedCommentId: BigInt(commentsId),
-    },
-  });
+  creator(id, commentsId, "liked", "likedCommentId");
 
   console.log("생성완료");
 
-  const count = await prisma.liked.count({
-    where: {
-      likedCommentId: commentsId,
-    },
-  });
+  const count = counter(commentsId, "liked", "likedCommentsId");
   console.log("count => ", count);
 
-  const updatedUser = await prisma.comments.update({
-    where: {
-      id: BigInt(commentsId),
-    },
-    data: {
-      like: BigInt(count),
-    },
-  });
-
+  const updatedUser = userUpdated(commentsId, "liked", "like", count);
   console.log("updatedUser => ", updatedUser);
 
   return res.status(201).json({ message: "좋아요가 추가 되었습니다." });
@@ -83,80 +158,82 @@ router.post("/boards/:boardId/recommend", AuthMiddleware, async (req, res) => {
   const { id } = req.user;
   const boardId = req.params.boardId;
 
-  const isExistrecom = await prisma.recom.findFirst({
-    where: {
-      userId: id,
-      recomedBoardId: boardId,
-    },
-  });
+  handleGood(id, boardId, "recom", "boards", "recomedBoardId", res, "추천");
 
-  console.log("isExistrecom => ", isExistrecom);
+  // const isExistrecom = await prisma.recom.findFirst({
+  //   where: {
+  //     userId: id,
+  //     recomedBoardId: boardId,
+  //   },
+  // });
 
-  if (!isExistrecom) {
-    await prisma.$transaction(async (tx) => {
-      await tx.recom.create({
-        data: {
-          userId: id,
-          recomedBoardId: BigInt(boardId),
-        },
-      });
+  // console.log("isExistrecom => ", isExistrecom);
 
-      console.log("생성완료");
+  // if (!isExistrecom) {
+  //   await prisma.$transaction(async (tx) => {
+  //     await tx.recom.create({
+  //       data: {
+  //         userId: id,
+  //         recomedBoardId: BigInt(boardId),
+  //       },
+  //     });
 
-      const count = await tx.recom.count({
-        where: {
-          recomedBoardId: boardId,
-        },
-      });
-      console.log("count => ", count);
+  //     console.log("생성완료");
 
-      const updatedBoard = await tx.boards.update({
-        where: {
-          id: BigInt(boardId),
-        },
-        data: {
-          recom: BigInt(count),
-        },
-      });
+  //     const count = await tx.recom.count({
+  //       where: {
+  //         recomedBoardId: boardId,
+  //       },
+  //     });
+  //     console.log("count => ", count);
 
-      console.log("updatedBoard => ", updatedBoard);
-    });
+  //     const updatedBoard = await tx.boards.update({
+  //       where: {
+  //         id: BigInt(boardId),
+  //       },
+  //       data: {
+  //         recom: BigInt(count),
+  //       },
+  //     });
 
-    return res.status(201).json({ message: "추천 되었습니다." });
-  } else {
-    await prisma.$transaction(async (tx) => {
-      await tx.recom.delete({
-        where: {
-          userId_recomedBoardId: {
-            userId: id,
-            recomedBoardId: BigInt(boardId),
-          },
-        },
-      });
+  //     console.log("updatedBoard => ", updatedBoard);
+  //   });
 
-      console.log("삭제완료");
+  //   return res.status(201).json({ message: "추천 되었습니다." });
+  // } else {
+  //   await prisma.$transaction(async (tx) => {
+  //     await tx.recom.delete({
+  //       where: {
+  //         userId_recomedBoardId: {
+  //           userId: id,
+  //           recomedBoardId: BigInt(boardId),
+  //         },
+  //       },
+  //     });
 
-      const count = await tx.recom.count({
-        where: {
-          recomedBoardId: boardId,
-        },
-      });
-      console.log("count => ", count);
+  //     console.log("삭제완료");
 
-      const updatedBoard = await tx.boards.update({
-        where: {
-          id: BigInt(boardId),
-        },
-        data: {
-          recom: BigInt(count),
-        },
-      });
+  //     const count = await tx.recom.count({
+  //       where: {
+  //         recomedBoardId: boardId,
+  //       },
+  //     });
+  //     console.log("count => ", count);
 
-      console.log("updatedBoard => ", updatedBoard);
-    });
+  //     const updatedBoard = await tx.boards.update({
+  //       where: {
+  //         id: BigInt(boardId),
+  //       },
+  //       data: {
+  //         recom: BigInt(count),
+  //       },
+  //     });
 
-    return res.status(400).json({ errorMessage: "추천을 취소 하였습니다" });
-  }
+  //     console.log("updatedBoard => ", updatedBoard);
+  //   });
+
+  //   return res.status(400).json({ errorMessage: "추천을 취소 하였습니다" });
+  // }
 });
 
 //유저팔로우
